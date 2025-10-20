2983bd2a5d24a068d7cdd05360f8934294a088c9
gabriel-barbos <gabriel.barbosa79@outlook.com>
2025-10-20 12:27:50 -0300

    fix: type error

--- server/services/utils.js
+++ server/services/utils.js
@@ -12,7 +12,7 @@ const produtoMap = {
   "2G - Com Bloqueio Padrão + RFID + Buzzer": ["Chicote Elétrico Padrão", "LEITOR DE CARTÃO RFID"],
   "2G - Com Bloqueio Padrão 24V": ["Chicote Elétrico Padrão"],
   "4G - Com Bloqueio + Buzzer": ["Chicote Elétrico Padrão"],
-  "2G - Sem Bloqueio + Buzzer": ["Chicote Elétrico Padrão"],
+  "2G - Sem bloqueio + Buzzer": ["Chicote Elétrico Padrão"],
   " 2G - Sem bloqueio + Buzzer": ["Chicote Elétrico Padrão"],
   "2G - Com Bloqueio + Buzzer": ["Chicote Elétrico Padrão"],
   "4G - Com Bloqueio Padrão": ["Chicote Elétrico Padrão"],
